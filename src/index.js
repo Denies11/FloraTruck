166ace3c70422acccfbfff6992066c272edff6a4
Arist0craft <dimnktn@gmail.com>
2021-12-20 13:20:43 +0300

    Изменил способ остлеживания загрузки скрипта Карт Change: функцию waitYMaps на initYmaps

--- src/index.js
+++ src/index.js
@@ -4,19 +4,21 @@ import initYandexMapsCallback from "./ymapsSettings";
 /**
  * Дожидаемся загрузки библиотеки ymaps из CDN, после чего инициализуруем карту
  */
-function waitYMaps() {
-  if (!window.ymaps) {
-    setTimeout(waitYMaps, 1);
-  } else {
+function initYmaps() {
+  const ymapsScript = document.createElement("script");
+  ymapsScript.type = "text/javascript"
+  ymapsScript.src = "https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=492e958b-e294-47cb-a1e5-c938260b6a0e";
+  ymapsScript.addEventListener("load", e => {
     ymaps.ready(initYandexMapsCallback);
-  }
+  })
+  document.head.appendChild(ymapsScript);
 }
 
 /**
  * Вход в приложение
  */
 function main() {
-  waitYMaps();
+  initYmaps();
 }
 
 main();
